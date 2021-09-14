a4900c4230008028c2283a4e557565225d10e82a
Opilane TTHK <opilane@tthk.ee>
2021-09-14 12:07:20 +0300

    add and or or

---
+++ BooLeanLogicalOperators/Program.cs
@@ -0,0 +1,32 @@
+﻿ using System;
+
+namespace BooLeanLogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja kautajatunnust
+            //programm küsib kasutaja parooli
+            //programm kontrollib kas mõlemad nii kasutajatunnus
+            //kui ka parool on õiged
+            //kui mõlemad kasutajatunnus ja parool
+            //kui kasutajatunnus või parool on vale, siis
+            //programm kuvab "sisselogimine ebaõnnestus. Proovi Uuesti."
+            //Kt: admin, pr:admin1234
+
+            Console.WriteLine("Sisesta kasutajatunnus");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta parool");
+            string UserPassword = Console.ReadLine();
+
+            if(userName =="admin" && UserPassword == "admin1234")
+            {
+                Console.WriteLine("sisselogimine ebaõnnestus, proovige uuesti");
+            }
+
+
+
+        }
+    }
+}
